f162182c934906c7a68cb413e22c0149c1155d40
saad raja <shraja7@gmail.com>
2023-07-03 22:54:34 -0500

    initialize navbar and footer

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/">shr.</Link>
+    </div>
+  );
+};
+
+export default Navbar;
